f0768d8cc284683b8a7ac20198d49ba4a7137e00
Aditya <adityagaikwad693@gmail.com>
2022-06-22 00:35:34 +1000

    Changes to the HTML

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
-var requestUrl = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=AUD,JPY,EUR&tryConversion=true'
+var requestUrlBTC = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=AUD,JPY,EUR&tryConversion=true'
+var requestUrlETH = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=AUD,JPY,EUR&tryConversion=true'
 var exchangeRate = document.querySelector('#rate');
-var buttonEl = document.querySelector('#button')
-console.log(buttonEl);
+var buttonEl = document.querySelector('#button');
+var cryptoOption = document.querySelector('#optionCrypto');
+console.log(cryptoOption);
 
 
 buttonEl.addEventListener('click',function(){
+    var requestUrl;
+    cryptoOption.value;
+    if(cryptoOption = 'BTC'){
+        requestUrl = requestUrlBTC;
+    }else{
+        console.log("this works")
+    }
     fetch(requestUrl)
     .then(function(r){
+        console.log(r)
         r.json()
         .then(function(data){
             console.log(data);
